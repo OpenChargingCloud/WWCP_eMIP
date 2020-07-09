a227a3ee66421014978188b0c9350dabe1d0d694
Achim 'ahzf' Friedland <achim.friedland@graphdefined.com>
2020-07-09 15:15:09 +0200

    Cleanups

--- WWCP_eMIP_Adapter/Tests/SetServiceAuthorisationRequest001.sh
+++
@@ -1,6 +0,0 @@
-curl -v -X POST -H "Content-Type:   application/soap+xml" \
-                -H "Accept:         application/soap+xml" \
-				-H "Authorization:  0815" \
-				-d @SetServiceAuthorisationRequest001.xml \
-				http://127.0.0.1:3004/RNs/Prod/IO/Gireve
-				

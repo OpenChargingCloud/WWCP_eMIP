c780d959db06033c12b698eaf50b4f3bfb72cf2f
Achim Friedland <achim.friedland@graphdefined.com>
2022-06-16 17:15:38 +0200

    Bugfixes

--- NET6/WWCP_eMIP_WebAPI/eMIPWebAPI.cs
+++ NET6/WWCP_eMIP_WebAPI/eMIPWebAPI.cs
@@ -302,7 +302,7 @@ namespace org.GraphDefined.WWCP.eMIPv0_7_4.WebAPI
 
             if (DisableLogging == false)
             {
-                Directory.CreateDirectory(Path.Combine(AppContext.BaseDirectory, this.LoggingPath));
+                Directory.CreateDirectory(this.LoggingPath);
             }
 
             this.DebugLog                = HTTPServer.AddJSONEventSource(EventIdentification:      DebugLogId,

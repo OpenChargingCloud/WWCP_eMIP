d997f835a09648527e051d4212e3e77dd7b1f98d
Achim Friedland <achim.friedland@graphdefined.com>
2023-09-06 03:52:54 +0200

    Minor documentation fix

--- WWCP_eMIP/CPO/CPOClient/CPOClient.cs
+++ WWCP_eMIP/CPO/CPOClient/CPOClient.cs
@@ -969,8 +969,8 @@ namespace cloud.charging.open.protocols.eMIPv0_7_4.CPO
         /// <param name="RemoteURL">The remote URL of the OICP HTTP endpoint to connect to.</param>
         /// <param name="VirtualHostname">An optional HTTP virtual hostname.</param>
         /// <param name="Description">An optional description of this CPO client.</param>
-        /// <param name="RemoteCertificateValidator">The remote SSL/TLS certificate validator.</param>
-        /// <param name="ClientCert">The SSL/TLS client certificate to use of HTTP authentication.</param>
+        /// <param name="RemoteCertificateValidator">The remote TLS certificate validator.</param>
+        /// <param name="ClientCert">The TLS client certificate to use of HTTP authentication.</param>
         /// <param name="HTTPUserAgent">The HTTP user agent identification.</param>
         /// <param name="RequestTimeout">An optional request timeout.</param>
         /// <param name="TransmissionRetryDelay">The delay between transmission retries.</param>
